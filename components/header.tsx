50ec4cc5b9ee421f1c854a83376a3e5c485564a3
azuemedzo <steeazue@gmail.com>
2025-04-30 15:27:04 -0700

    Header

--- components/header.tsx
+++ components/header.tsx
@@ -1,12 +1,14 @@
 import React from "react";
 import Link from "next/link";
+import Image from "next/image";
 
 export default function  Header() {
  return(
     <header className="bg-green-800 text white px-6 py-4 flex justify-between items -center shadow">
         <div className="text-2xl font-bold">
             <h1>PAYWISE</h1>
-            <img src="/logo" alt="logo" className="rounded-4xl text-yellow-300 justify items font-green-500" />
+            <Image src="/logo" width={24}
+      height={24} alt="logo" className="rounded-4xl text-yellow-300 justify items font-green-500" />
         </div>
          <nav className="hidden md:flex space-x-8 text sm font-medium">
             <Link href="#"className="hover:text-yellow-300 transition">
