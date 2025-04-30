7759eb08f03ca8693a8f7351761edc328162c4ae
azuemedzo <steeazue@gmail.com>
2025-04-30 15:06:16 -0700

    Header

--- components/header.tsx
+++ components/header.tsx
@@ -1,9 +1,35 @@
+import React from "react";
+import Link from "next/link";
 
-
-export default function Header() {
-    return (
-        <header>
-        
+export default function  Header() {
+ return(
+    <header className="bg-green-800 text white px-6 py-4 flex justify-between items -center shadow">
+        <div className="text-2xl font-bold">
+            <h1>PAYWISE</h1>
+            <img src="/logo" alt="logo" className="rounded-4xl text-yellow-300 justify items font-green-500" />
+        </div>
+         <nav className="hidden md:flex space-x-8 text sm font-medium">
+            <Link href="#"className="hover:text-yellow-300 transition">
+                Home
+            </Link>,
+            <Link href="#"className= "hover:text-yellow-300 transition">
+                Features
+            </Link>,
+            <Link href="#"className= "hover:text yellow-300 transition">
+                Cards
+            </Link>,
+            <Link href="#"className= "hover:text-yellow-300 transition">
+                Contact
+            </Link>,
+        </nav>
+                <div className="flex space-x-4">
+                    <button className=" hover:text-yellow-300">
+                        Log in
+                    </button>
+                    <button className="bg-yellow-400 text-green-900 px-4 py-2 rounded font-semibold hover:bg-yellow-300 transition">
+                        Sign up
+                    </button>
+                </div>
         </header>
     )
 }
--- components/hero.tsx
+++ components/hero.tsx
@@ -1,11 +1,4 @@
-import { Button } from "@/components/ui/button"
 
-export default function hero() {
-  return (
-    <section>
-      hero
-      <Button variant="outline">Button</Button>
+    
 
-    </section>
-  )
-}
+
